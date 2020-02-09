96004e94e6bd75b4c3097b51b5fb911ccaf6148e
hello <vijjus1311@gmail.com>
2020-02-10 01:03:13 +0530

    hey

---
+++ src/test/java/login/sample1.java
@@ -0,0 +1,10 @@
+package login;
+
+public class sample1 
+{
+	public static void main(String[] args) 
+	{
+	System.out.println("welcome to seleneimu class......");	
+	}
+
+}
---
+++ src/test/java/login/sample2.java
@@ -0,0 +1,10 @@
+package login;
+
+public class sample2 
+{
+	public static void main(String[] args) 
+	{
+	System.out.println("welcome to seleneimu class......");	
+	}
+
+}
